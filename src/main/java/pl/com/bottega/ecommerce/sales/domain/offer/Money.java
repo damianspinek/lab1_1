4fd47ab8c5f4858e690f4cc20d0e2c5b54857ada
Damian Spinek <damian00737@gmail.com>
2015-03-11 20:03:06 +0100

    Dodanie akcesorów do klasy Money

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
@@ -5,5 +5,27 @@ import java.math.BigDecimal;
 public class Money {
 	private BigDecimal value;
 	private String currency;
+	public BigDecimal getValue() {
+		return value;
+	}
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+	public String getCurrency() {
+		return currency;
+	}
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+	public int compareTo(Money totalCost) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	public BigDecimal subtract(Money min) {
+		return this.value.min(min.value);
+	}
+	public BigDecimal multiply(BigDecimal bigDecimal) {
+		return this.value.multiply(bigDecimal);
+	}
 	
 }
